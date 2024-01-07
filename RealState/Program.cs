0e6cdb871026178cf7b69915e4f5091f45fb59bc
DESKTOP-2H84IA0\Cesar <cesarlozanolahoz@gmail.com>
2024-01-07 03:17:04 +0100

    - Installer project added to solution - SQLite database file added to installer - Log4net properly configured in App.config file

--- RealState/Program.cs
+++ RealState/Program.cs
@@ -1,4 +1,6 @@
-﻿using RealState.Forms;
+﻿using log4net;
+using log4net.Config;
+using RealState.Forms;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -10,12 +12,20 @@ namespace RealState
 {
     internal static class Program
     {
+        // Define a static logger variable so that it references the
+        // Logger instance named "Program".
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            XmlConfigurator.Configure();
+
+            log.Debug("Application Started");
+
             SQLiteManager sqliteManager = new SQLiteManager(ConfigurationManager.ConnectionStrings["SQLiteConnectionString"]?.ConnectionString);
 
             Application.EnableVisualStyles();
